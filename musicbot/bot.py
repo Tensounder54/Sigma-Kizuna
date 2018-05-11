3ac933a1d2449863514158ed161d35c5e7321507
Isaiah Ho <isaiahho@live.com>
2018-05-11 16:43:02 -0400

    Fix retain messages

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -3734,8 +3734,8 @@ class MusicBot(discord.Client):
 
                 sentmsg = await self.safe_send_message(
                     message.channel, content,
-                    expire_in=response.delete_after if self.config.delete_messages else 0,
-                    also_delete=message if self.config.delete_invoking else None
+                    expire_in=response.delete_after if self.config.delete_messages and command not in self.config.retain_commands else 0,
+                    also_delete=message if self.config.delete_invoking and command not in self.config.retain_commands else None
                 )
 
         except (exceptions.CommandError, exceptions.HelpfulError, exceptions.ExtractionError) as e:
